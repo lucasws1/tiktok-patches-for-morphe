277f4003643b35045b62f9f95f63ed2d8bb889bf
just-a-weird-guy <bagirahmed5@gmail.com>
2026-06-05 01:10:25 +0400

    fix: restore downloads fallback for restricted videos

--- extensions/tiktok/stub/src/main/java/com/ss/android/ugc/aweme/base/model/UrlModel.java
+++ extensions/tiktok/stub/src/main/java/com/ss/android/ugc/aweme/base/model/UrlModel.java
@@ -4,7 +4,19 @@ import java.util.List;
 
 @SuppressWarnings("unused")
 public class UrlModel {
+    public String getUri() {
+        throw new UnsupportedOperationException("Stub");
+    }
+
+    public String getUrlKey() {
+        throw new UnsupportedOperationException("Stub");
+    }
+
     public List<String> getUrlList() {
+        throw new UnsupportedOperationException("Stub");
+    }
+
+    public long getSize() {
         throw new UnsupportedOperationException("Stub");
     }
 }
--- extensions/tiktok/stub/src/main/java/com/ss/android/ugc/aweme/feed/model/Video.java
+++ extensions/tiktok/stub/src/main/java/com/ss/android/ugc/aweme/feed/model/Video.java
@@ -1,8 +1,10 @@
 package com.ss.android.ugc.aweme.feed.model;
 
+import com.ss.android.ugc.aweme.base.model.UrlModel;
+
 @SuppressWarnings("unused")
 public class Video {
-    public VideoUrlModel downloadNoWatermarkAddr;
+    public UrlModel downloadNoWatermarkAddr;
     public VideoUrlModel h264PlayAddr;
     public VideoUrlModel playAddr;
 }
